90e6c3a16829a0ea4d5a4f172148ec84249f7299
hiropa <martin.kunze@gmail.com>
2022-02-09 12:16:16 +0100

    Forked from  jmez11 / rclone-addon to include binary as download is no longer working

--- script.service.rclone/main.py
+++ script.service.rclone/main.py
@@ -1,19 +1,82 @@
 #!/usr/bin/python3.4
-import os, sys, xbmc, time, stat, xbmcvfs, xbmcaddon
+from __future__ import absolute_import, division, unicode_literals
+from future import standard_library
+from future.builtins import *
+standard_library.install_aliases()
 
-src = os.path.join(xbmcaddon.Addon().getAddonInfo('path'), 'rclone-android-16-arm')
-loc = xbmcvfs.translatePath("special://xbmcbin/../../../cache/lib/rclone-android-16-arm")
+import os, sys, xbmc, time, stat, xbmcvfs, xbmcaddon, xbmcplugin, xbmcgui, gzip, subprocess, urllib.request
+
+sourceurl = xbmcaddon.Addon().getSetting("rclonedownload")
+
+PY3 =  sys.version_info > (3, 0)
+if PY3:
+	zippath  = xbmcvfs.translatePath("special://temp/rclone.gz")
+	loc = xbmcvfs.translatePath("special://xbmcbin/../../../cache/lib/rclone-android-16-arm")
+	locwin = xbmcvfs.translatePath("special://xbmcbin/rclone.exe")
+	loc2 = xbmcvfs.translatePath("special://masterprofile/rclone.conf")
+	pidfile  = xbmcvfs.translatePath("special://temp/librclone.pid")
+	logfile  = xbmcvfs.translatePath("special://temp/librclone.log")
+	cachepath  = xbmcvfs.translatePath("special://temp") 
+else:
+	zippath  = xbmc.translatePath("special://temp/rclone.gz")
+	loc = xbmc.translatePath("special://xbmcbin/../../../cache/lib/rclone-android-16-arm")
+	locwin = xbmc.translatePath("special://xbmcbin/rclone.exe")
+	loc2 = xbmc.translatePath("special://masterprofile/rclone.conf")
+	pidfile  = xbmc.translatePath("special://temp/librclone.pid")
+	logfile  = xbmc.translatePath("special://temp/librclone.log")
+	cachepath  = xbmc.translatePath("special://temp") 	
+
+if os.name == 'nt':
+	loc = locwin
 
 if not xbmcvfs.exists(loc):
-    xbmcvfs.copy(src, loc)
-    st = os.stat(loc)
-    os.chmod(loc, st.st_mode | stat.S_IEXEC)
+	progress_bar = xbmcgui.DialogProgressBG()
+	progress_bar.create('Download', '')
 
-loc2 = xbmcvfs.translatePath("special://masterprofile/rclone.conf")
-pidfile  = xbmcvfs.translatePath("special://temp/librclone.pid")
-logfile  = xbmcvfs.translatePath("special://temp/librclone.log")
-cachepath  = xbmcvfs.translatePath("special://temp")
+	def reporthook(block_number, block_size, total_size):
+		if 0 == block_number & 511:
+			 percent = (block_number * block_size * 100) / total_size
+			 progress_bar.update(int(percent))
+			 
+	urllib.request.urlretrieve(sourceurl, zippath, reporthook)
+	progress_bar.close()
+	input = gzip.GzipFile(zippath, 'rb')
+	s = input.read()
+	input.close()
+	output = open(loc, 'wb')
+	output.write(s)
+	output.close()
+	st = os.stat(loc)
+	os.chmod(loc, st.st_mode | stat.S_IEXEC)
+
+command = xbmcaddon.Addon().getSetting("parameters")
+
+def run(cmd):
+	os.environ['PYTHONUNBUFFERED'] = "1"
+	if os.name == 'nt':
+		proc = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE, universal_newlines = True  )
+	else:
+		proc = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE, universal_newlines = True, shell = True  )
+	xbmc.log(msg='RCLONE: Executing ' + cmd, level=xbmc.LOGINFO)
+	stdout = []
+	stderr = []
+	mix = []
+	while proc.poll() is None:
+		line = proc.stdout.readline()
+		if line != "":
+			stdout.append(line)
+			mix.append(line)
+			xbmc.log(msg='RCLONE:' + line, level=xbmc.LOGINFO)
+		line = proc.stderr.readline()
+		if line != "":
+			stderr.append(line)
+			mix.append(line)
+			xbmc.log(msg='RCLONE:' + line, level=xbmc.LOGINFO)
+	return proc.returncode, stdout, stderr, mix
 
 while True:
-    os.popen(loc + " serve webdav onedrive: --addr 127.0.0.1:23457 --config " + loc2 + " --log-file=" + logfile + " --dir-cache-time 2400h --poll-interval 10m")
-    break
+	if os.name == 'nt':
+		code, out, err, mix = run("\"" + loc + "\" " + command + " --config \"" + loc2 + "\" --log-file \"" + logfile + "\" --cache-dir \"" + cachepath + "\"")
+	else:
+		code, out, err, mix = run(loc + " " + command + " --config " + loc2 + " --log-file=" + logfile + " --cache-dir " + cachepath + " &")
+	time.sleep(2)
